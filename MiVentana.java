52be7f6f66761a1e81a9ffdd7bcfb99da4854c62
Fortunato <fortunato.ramirez@tectijuana.edu.mx>
2020-06-16 11:50:36 -0700

    version con dos botones

--- MiVentana.java
+++ MiVentana.java
@@ -6,6 +6,7 @@ class MiVentana extends JFrame{
 		JPanel panel;
 		JLabel lblTexto1;
 		JButton btnBoton1;
+		JButton btnBoton2;
 
 		panel = new JPanel();
 		panel.setLayout(null);
@@ -13,9 +14,12 @@ class MiVentana extends JFrame{
 		lblTexto1.setBounds(50, 40, 200, 10); //(x, y, w, h)
 		btnBoton1 = new JButton("OK");
 		btnBoton1.setBounds(50, 100, 100, 30);
+		btnBoton2 = new JButton("OK 2");
+		btnBoton2.setBounds(50, 150, 100, 30);
 
 		panel.add(lblTexto1);
 		panel.add(btnBoton1);
+		panel.add(btnBoton2);
 
 		this.add(panel);
 		this.setBounds(400,400,500,350);
