c448efb3d5442cd8f3cb6aa322880bc9d97490d4
Ismael <1leamsismael1@gmail.com>
2020-06-18 11:25:13 -0700

    Nuevas ventanas emergentes

---
+++ VentanaEmergente3.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+
+class VentanaEmergente3 extends JFrame{
+
+	JLabel lbl1;
+	JPanel panel;
+
+	public VentanaEmergente3()
+	{
+
+
+		panel= new JPanel();
+		panel.setLayout(null);
+		panel.setBounds(0,0,400,200);
+		panel.setVisible(true);
+
+		lbl1= new JLabel("Hola, si funicona jeje");
+		lbl1.setBounds(30,30,450,50);
+
+		panel.add(lbl1);
+
+		this.setTitle("Isma");
+		this.setSize(500, 200);
+		this.setDefaultCloseOperation(this.DISPOSE_ON_CLOSE);
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		this.add(panel);
+	}
+
+}
